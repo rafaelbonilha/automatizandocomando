273869eca4fe91f472cc6fe183186100aa1da13c
rafaelbonilha <jrbonilha@hotmail.com>
2026-03-15 19:00:56 -0300

    Update scrllk.ps1
    ajustando o script e adicionando nota de informação

--- scrllk.ps1
+++ scrllk.ps1
@@ -1,5 +1,21 @@
-# Comando para simular o uso do botão Scroll Lock por um determinado período de tempo
-# Curso de PowerShell
+<#
+.SYNOPSIS
+Comando para simular o uso do botão Scroll Lock por um determinado período de tempo
+
+
+.NOTES
+Execute o script em uma sessão PowerShell iniciada com -STA com o comando a seguir:
+powershell -STA -File Diretorio\scrllk.ps1 ou .\scrllk.ps1
+
+Caso de Uso.:
+
+Basico.: .\scrllk.ps1 - Comando para simular o uso do botão Scroll Lock por um determinado período de tempo
+
+Autor.: Joao Rafael F. Bonilha - Curso de PowerShell
+
+ATENÇÃO.: Script para estudos de powershell, so use em ambiente de testes/lab.
+
+#>
 
 Clear-Host
 Write-Host "Teste de pressionamento do botão Scroll Lock." -ForegroundColor Red
